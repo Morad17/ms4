58df0936fadc50bf8265480f10b3c2bcc95250da
Morad17 <morad123@hotmail.co.uk>
2020-12-23 11:27:51 +0000

    added fixtures/products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+# Registered Category and Product Models for Admin View
+admin.site.register(Product)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    #String method
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    gender = models.CharField(max_length=10, blank=False, default='Unisex')
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
