373ed04cd66b6ec41bff0421478d1b8ebb56dc54
Morad17 <morad123@hotmail.co.uk>
2020-12-27 21:52:35 +0000

    sorting out product detail page/ incrementing and decrementing products

--- products/models.py
+++ products/models.py
@@ -17,13 +17,15 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
+
+    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
+
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=150)
     description = models.TextField()
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women')('U','Unisex'))
-    gender = models.CharfieldField(choices=GENDER_CHOICES, blank=False, default='Unisex')
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
