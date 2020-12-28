41d3ca5211c57c6ace5699ca26abef65719ca813
Morad17 <morad123@hotmail.co.uk>
2020-12-28 18:21:42 +0000

    updated bag update and remove views.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
@@ -9,17 +10,21 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity *product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
-
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+    if not len(bag.items()):
+        delivery = 0
+        total = 0
+        free_delivery_cap = settings.FREE_DELIVERY_THRESHOLD - total
+    elif total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total + settings.STANDARD_DELIVERY_COST
         free_delivery_cap = settings.FREE_DELIVERY_THRESHOLD - total
     else:
@@ -37,6 +42,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
 
     return context
--- products/views.py
+++ products/views.py
@@ -58,6 +58,7 @@ def all_products(request):
 def product_detail(request, product_id):
     """The view to show individual product details"""
 
+    
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
