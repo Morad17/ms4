712bb3a6b9b33c5b23027d561342416027cf651d
Morad17 <morad123@hotmail.co.uk>
2020-12-31 21:22:07 +0000

    updates on settings

--- ms4/settings.py
+++ ms4/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sk_test_51I3s7wIeCsFt2asbD0nxSevqgpNjpW4XzT5jBco1jJoGHoa2PKTuTgSJ0dfhRA6eJyNX6MJO6NnjZ80kmmg1khFe00mo1sgyvN'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['afk-gamer.herokuapp.com', 'localhost']
 
