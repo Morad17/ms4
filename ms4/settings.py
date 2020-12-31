44cb1b88c3ca7fa9badfbd680b6c57f69eb915d1
Morad17 <morad123@hotmail.co.uk>
2020-12-31 21:16:08 +0000

    deplying to heroku

--- ms4/settings.py
+++ ms4/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'sk_test_51I3s7wIeCsFt2asbD0nxSevqgpNjpW4XzT5jBco1jJoGHoa2PKTuTgSJ0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['afk-gamer.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,6 +125,18 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
