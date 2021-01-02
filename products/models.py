38b0b031092a37661ed77606eecb2d35668c53e4
Morad17 <morad123@hotmail.co.uk>
2021-01-02 17:48:05 +0000

    Heroku Models Fix

--- products/models.py
+++ products/models.py
@@ -19,12 +19,11 @@ class Category(models.Model):
 #All the products for sale
 class Product(models.Model):
 
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=150)
     description = models.TextField()
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
+    gender = models.CharField(max_length=10, blank=False, default='Unisex')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -36,13 +35,12 @@ class Product(models.Model):
 #All Seasonal Products
 class Seasonal(models.Model):
 
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=150)
     description = models.TextField()
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
-    date = models.CharField(max_length=10, null=True, blank=True)
+    gender = models.CharField(max_length=10, blank=False, default='Unisex')
+    date = models.CharField(max_length=100, null=True, blank=True, default='January')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -54,12 +52,11 @@ class Seasonal(models.Model):
 #All Special Offer Products
 class Specials(models.Model):
 
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=150)
     description = models.TextField()
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
+    gender = models.CharField(max_length=10, blank=False, default='Unisex')
     rrp = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
