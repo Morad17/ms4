565d0213dff7b773061e26f1d13c1d12f37d7084
Morad17 <morad123@hotmail.co.uk>
2021-01-02 19:03:42 +0000

    Remigration

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('hot/', views.hot_products, name='hot_products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-
+    path('<int:specials_id>/', views.specials_detail, name='specials_detail'),
+    path('<int:seasonal_id>/', views.seasonal_detail, name='seasonal_detail'),
     
 ]
