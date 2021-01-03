caa6ca9d8cbb6f705b423472272039743fbb5cf9
Morad17 <morad123@hotmail.co.uk>
2021-01-03 21:32:11 +0000

    sorted out models errors and fixed hot items so it goes to product details

--- products/models.py
+++ products/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 #All the products for sale
 class Product(models.Model):
 
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
+    GENDER_CHOICES = [('Men', 'Men'),('Women', 'Women'),('Unisex','Unisex')]
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=150)
@@ -29,42 +29,21 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    season_sales = models.BooleanField(blank=False, default=False)
+    rrp = models.DecimalField(max_digits=6, decimal_places=2, null=True)
 
     def __str__(self):
         return self.name
 
-#All Seasonal Products
-class Seasonal(models.Model):
+#For Discounted and Seasonal Products
+class Season(models.Model):
 
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
-    date = models.CharField(max_length=10, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-#All Special Offer Products
-class Specials(models.Model):
-
-    GENDER_CHOICES = (('M', 'Men'),('W', 'Women'),('U','Unisex'))
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=False, default='Unisex')
-    rrp = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    ALL_SEASONS = [('winter','Winter'),('spring','Spring'),('summer','Summer'),('autumn','Autumn')]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    season = models.CharField(choices=ALL_SEASONS, max_length=10, blank=False )
+    month_start = models.CharField(max_length=150)
+    month_finish = models.CharField(max_length=150)
+    discount = models.DecimalField(max_digits=6, decimal_places=2, null=True)
 
     def __str__(self):
         return self.name
--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('hot/', views.hot_products, name='hot_products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('<int:specials_id>/', views.specials_detail, name='specials_detail'),
-    path('<int:seasonal_id>/', views.seasonal_detail, name='seasonal_detail'),
     
 ]
